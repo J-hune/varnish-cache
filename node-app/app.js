526c14ebd8c2c451c7f6293adf26224e31f6b1ae
J-hune <donovann@jhune.dev>
2023-10-31 01:27:54 +0100

    feat(node): Display time with EJS for cache behavior
    This commit focuses on improving the user experience and cache demonstration. The updates encompass:
    
    - Modified the "/test" endpoint in the Node.js application to return the current time in HH:mm:ss format.
    - Integrated the EJS templating engine in the Node.js application to render the time on the web page, illustrating cache stale-while-revalidate behavior.

--- node-app/app.js
+++ node-app/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();  // Create an Express application instance.
 const port = 8080;  // The port on which this application is listening.
 
+// Use ejs as the templating engine.
+app.engine('html', require('ejs').renderFile);
+
 // Middleware to enable Cross-Origin Resource Sharing (CORS)
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
@@ -16,23 +19,27 @@ app.use(express.static('public'));
 app.get('/', async (req, res) => {
    // Use the fetch API to make an asynchronous HTTP request to an external resource.
    const response = await fetch("http://" + process.env.BACKEND_DOMAIN_NAME + '/test');
+   const time = await response.text();
 
    // Check if the response headers indicate a cached response.
    if (response.headers.get("x-cache") === "HIT") {
-      res.sendFile('/views/cache.html', {root: __dirname});
+      res.render(__dirname + '/views/cache.html', {root: __dirname, time: time});
       console.log(`Answered cached request (${response.headers.get("x-varnish")})`);
    }
 
    // Serve the default HTML file if the response is not cached.
    else {
-      res.sendFile('/views/index.html', {root: __dirname});
+      res.render(__dirname + '/views/index.html', {root: __dirname, time: time});
       console.log('Answered request');
    }
 });
 
 // Define a route for '/test'.
 app.get('/test', async (req, res) => {
-   res.sendStatus(200);  // Send a 200 OK response.
+
+   // Get the current time and send it as a response.
+   const time = new Date().toTimeString().split(' ')[0];
+   res.send(time);
 });
 
 // Start the Express application and listen on the specified port.
